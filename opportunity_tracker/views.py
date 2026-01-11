db63b0ebdd6cbf638ab6fa7b822d606bcec26211
Yosef Sheinfil <scedastic@users.noreply.github.com>
2026-01-11 11:56:18 -0500

    more related info on detail page

--- opportunity_tracker/views.py
+++ opportunity_tracker/views.py
@@ -1,5 +1,5 @@
 import datetime
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.shortcuts import get_object_or_404, render
 from .models import Opportunity, Notes, FollowUp, Contact
 
@@ -17,7 +17,7 @@ def open_opportunities(request):
     
 def all_opportunities(request):
     """Show all open opportunities, open and expired."""
-    opportunities = Opportunity.objects.all().order_by("stage")
+    opportunities = Opportunity.objects.all().order_by("stage").order_by("company_name")
     return render(
         request,
         "opportunities.html",
@@ -28,10 +28,31 @@ def opportunity_view(request, opportunity_id):
     """Details about a specific opportunity"""
     opportunity = get_object_or_404(Opportunity, pk=opportunity_id)
     notes = Notes.objects.filter(opportunity=opportunity)
+    follow_ups = FollowUp.objects.filter(opportunity=opportunity)
+    contacts = Contact.objects.filter(opportunity=opportunity)
     return render(
-        request, "opportunity_detail.html", {"o": opportunity, "notes": notes}
+        request, "opportunity_detail.html", 
+        {
+            "page_title": "Opportunity Details", 
+            "o": opportunity, 
+            "follow_ups": follow_ups,
+            "contacts": contacts,
+            "notes": notes}
     )
 
+def opportunities_missing_contacts_follow_ups(request):
+    opportunities = ( 
+        Opportunity.objects.annotate(
+            contact_count=Count("contact"), 
+            note_count=Count("notes")
+        ).filter(contact_count=0, note_count=0, open=True)
+        .order_by("-initiation_date") 
+    )
+    return render(
+        request, 
+        "opportunities.html",
+        {"page_title": "Opportunities missing Contacts and Follow Ups", "opportunities": opportunities}
+    )
 
 def current_follow_ups(request):
     """Show all FollowUps for the next 7 days."""
