b550d1775cef7d58ae999a17e77a800e39d9fba4
Yosef Sheinfil <scedastic@users.noreply.github.com>
2026-01-30 13:36:49 -0500

    add contacts page; update stage

--- opportunity_tracker/urls.py
+++ opportunity_tracker/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from .views import (
     all_opportunities, open_opportunities, opportunities_missing_contacts_follow_ups, 
-    opportunity_view, 
+    opportunity_view, update_opportunity_stage,
+    all_contacts, current_contacts,
     current_follow_ups, complete_follow_up,
     )
 
@@ -25,7 +26,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", open_opportunities, name="open-opportunities"),
     path("opportunity/<int:opportunity_id>/", opportunity_view, name="opportunity_view"),
+    path("opportunity/<int:opportunity_id>/update-stage/<int:stage_id>/", update_opportunity_stage, name="update-opportunity-stage"),
     path("all/", all_opportunities, name="all-opportunities"),
+    path("contacts/", all_contacts, name="all-contacts"),
+    path("current-contacts/", current_contacts, name="current-contacts"),
     path("opp-no-cf/", opportunities_missing_contacts_follow_ups, name="opportunites-without-contacts-follow-ups"),
     path("followups/", current_follow_ups, name="current-follow-ups"),
     path("follow-up/<int:follow_up_id>/completed/", complete_follow_up, name="complete-follow-up"),
