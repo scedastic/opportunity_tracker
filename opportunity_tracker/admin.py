db25ae5ad9db77988962c868701e4ea2a6c6f285
Yosef Sheinfil <scedastic@users.noreply.github.com>
2026-05-05 13:01:43 -0400

    show company

--- opportunity_tracker/admin.py
+++ opportunity_tracker/admin.py
@@ -27,7 +27,7 @@ class NotesAdmin(admin.ModelAdmin):
 
 
 class FollowUpAdmin(admin.ModelAdmin):
-    list_display = ('opportunity','follow_up_date','completed')
+    list_display = ('company','opportunity','follow_up_date','completed')
     list_filter = ('completed',)
 
 
